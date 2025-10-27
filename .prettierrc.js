71c620328e1b52932e11ea7527090c0211439ab8
aldeen <71447171+aldeen1@users.noreply.github.com>
2025-10-27 12:00:23 +0700

    feat: add linter

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 4,
+  semi: true,
+  singleQuote: true,
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: "always",
+  endOfLine: "lf",
+  plugins: ["prettier-plugin-tailwindcss"]
+};
