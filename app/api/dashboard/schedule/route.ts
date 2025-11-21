1f97a44bc5704aa45bb3ef4684dcecac670f8ec1
NotMyTata <tata.wiropati@gmail.com>
2025-11-21 14:22:20 +0700

    fix: added missing parameter in dashboard route

--- app/api/dashboard/schedule/route.ts
+++ app/api/dashboard/schedule/route.ts
@@ -1,12 +1,13 @@
 import GetCurrentUserID from '@/src/application/usecases/GetCurrentUserID';
 import GetNewestScheduleListByUserID from '@/src/application/usecases/GetNewestScheduleListByUserID';
 import ScheduleListRepositoryImpl from '@/src/infrastructure/repositories/ScheduleListRepositoryImpl';
+import UserRepositoryImpl from '@/src/infrastructure/repositories/UserRepositoryImpl';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
         const scheduleList = await GetNewestScheduleListByUserID(
-            await GetCurrentUserID(),
+            await GetCurrentUserID(new UserRepositoryImpl()),
             new ScheduleListRepositoryImpl(),
         );
 
